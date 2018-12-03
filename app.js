77b24016b21cf9bebc7bee1e7614e4a598c32db9
Carlos Gómez <38505929+kdotdot@users.noreply.github.com>
2018-12-03 17:38:55 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -9,13 +9,9 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public'));
 
-app.get('/', function(req, res) {
-  res.send("Hello")
-});
-
 //fire controllers
 todoController(app);
 
 //listen to port
-app.listen(3000);
-console.log('Listening to port 3000');
+app.listen(process.env.PORT || 3000);
+console.log("Listening on port:" + process.env.PORT || 3000);
